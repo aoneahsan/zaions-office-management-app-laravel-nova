a741aac62ae81eff44129c2fd9fe363805318c57
aoneahsan <aoneahsan@gmail.com>
2023-05-18 17:44:06 +0500

    p-starting-point-v1 - updated

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,6 +19,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // To find migration files in sub folders - Code Start
+        $mainPath = database_path('migrations');
+        $directories = glob($mainPath . '/*', GLOB_ONLYDIR);
+        $paths = array_merge([$mainPath], $directories);
+
+        $this->loadMigrationsFrom($paths);
+        // To find migration files in sub folders - Code End
     }
 }
